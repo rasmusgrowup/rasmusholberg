a82899750c03f9bda06e0ecb8b4b92b9ee3f5984
Rasmus Andersen <rasmus@growupstudio.dk>
2023-02-18 17:14:57 +0100

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,11 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,opsz@1,8..60&display=swap" rel="stylesheet" />
+                    <link rel="preload" href="/dabea.jpg" as="image" />
+                    <link rel="preload" href="/dogme.jpg" as="image" />
+                    <link rel="preload" href="/organic_alt.jpg" as="image" />
+                    <link rel="preload" href="/tinekhome.jpg" as="image" />
+                    <link rel="preload" href="/twelveinch.jpg" as="image" />
                 </Head>
                 <body>
                 <Main />
