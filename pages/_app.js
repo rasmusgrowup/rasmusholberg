8bfa05952b446d81539c61936275c51c73cf759c
Rasmus Andersen <rasmus@growupstudio.dk>
2023-05-08 13:09:11 +0200

    added vercel anonymous analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import MouseContextProvider from "../lib/MouseContext";
+import {Analytics} from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <MouseContextProvider>
-      <Component {...pageProps} />
-    </MouseContextProvider>
-  )
+function MyApp({Component, pageProps}) {
+    return (
+        <MouseContextProvider>
+            <Component {...pageProps} />
+            <Analytics/>
+        </MouseContextProvider>
+    )
 }
 
 export default MyApp
